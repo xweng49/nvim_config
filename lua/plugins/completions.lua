a43c52cb1b285c5064bbf38d00a3ab32de303efa
Xing Weng <xweng49@gmail.com>
2025-07-21 17:45:40 -0500

    update to use blink cmp for autocompletion

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,65 +1,44 @@
 return {
-	{
-		"hrsh7th/cmp-nvim-lsp",
-	},
-
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
+	"saghen/blink.cmp",
+	dependencies = {
+		{
+			"L3MON4D3/LuaSnip",
+			version = "v2.*",
 		},
 	},
-	{
-		"hrsh7th/nvim-cmp",
-		config = function()
-			local cmp = require("cmp")
-			require("luasnip.loaders.from_vscode").lazy_load()
+	version = "v1.*",
+	---@module "blink.cmp"
+	---@type blink.cmp.Config
+	opts = {
+		signature = { enabled = true },
+		appearance = {
+			use_nvim_cmp_as_default = false,
+			nerd_font_variant = "mono",
 
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-				--window = {
-				--	completion = cmp.config.window.bordered(),
-				--	documentation = cmp.config.window.bordered(),
-				--},
-				--mapping = cmp.mapping.preset.insert({
-				--	["<C-B>"] = cmp.mapping.scroll_docs(-4),
-				--	["<C-f>"] = cmp.mapping.scroll_docs(3),
-				--	["<C-Space>"] = cmp.mapping.complete(),
-
-				--	["<CR>"] = cmp.mapping.confirm({ select = true }),
-				--}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-					{ name = "buffer" },
-				}),
-			})
-		end,
-	},
-	{
-		"saghen/blink.cmp",
-		dependencies = { "rafamadriz/friendly-snippets" },
-		version = "1.*",
-		-- AND/OR build from 		---@module 'blink.cmp'
-		---@type blink.cmp.Config
-		opts = {
-			keymap = { preset = "default" },
-			appearance = {
-				--	use_nvim_cmp_as_default = true,
-				nerd_font_variant = "mono",
-			},
-			completion = { documentation = { auto_show = false } },
-			sources = {
-				default = { "lsp", "path", "snippets", "buffer" },
-			},
-			fuzzy = { implementation = "prefer_rust_with_warning" },
-			signature = { enabled = true },
 		},
-		opts_extend = { "sources.default" },
+
+		completion = { documentation = { auto_show = true} },
+		sources = {
+			default = {
+				"lsp",
+				"path",
+				"snippets",
+				"buffer",
+			},
+		},
+		snippets = {
+			preset = "luasnip",
+		},
 	},
+--	opts_extend = {
+--		"sources.default",
+--	},
+	config = function(_, opts)
+		local lspkind = require('lspkind')
+		
+
+		require("luasnip.loaders.from_vscode").lazy_load()
+		require("luasnip.loaders.from_lua").load({ paths = "./lua/luasnippets" })
+		require("blink.cmp").setup(opts)
+	end,
 }
