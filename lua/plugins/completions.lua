c68089cb8cb47cc422ee04c9cb4a1db4229e0eba
Xing Weng <xweng49@gmail.com>
2025-09-06 15:32:02 -0500

    fixed completion error

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -36,33 +36,40 @@ return {
 					"snippets",
 					"buffer",
 				},
-				{
-					"folke/lazydev.nvim",
-					ft = "lua", -- only load on lua files
-					opts = {
-						library = {
-							-- See the configuration section for more details
-							-- Load luvit types when the `vim.uv` word is found
-							{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
-						},
+				providers = {
+					lazydev = {
+						name = "LazyDev",
+						module = "lazydev.integrations.blink",
+						score_offset = 100, -- show at a higher priority than lsp
 					},
 				},
+				--	{
+				--		"folke/lazydev.nvim",
+				--		ft = "lua", -- only load on lua files
+				--		opts = {
+				--			library = {
+				--				-- See the configuration section for more details
+				--				-- Load luvit types when the `vim.uv` word is found
+				--				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+				--			},
+				--		},
+				--	},
 			},
-			snippets = {
-				preset = "luasnip",
-			},
-			snippet = {
-				expand = function(args)
-					require("luasnip").lsp_expand(args.body)
-				end,
-			},
+			--snippets = {
+			--	preset = "luasnip",
+			--},
+			--snippet = {
+			--	expand = function(args)
+			--		require("luasnip").lsp_expand(args.body)
+			--	end,
+			--},
 		},
-		--	opts_extend = {
-		--		"sources.default",
-		--	},
+		--		opts_extend = {
+		--			"sources.default",
+		--		},
 		config = function(_, opts)
 			require("luasnip.loaders.from_vscode").lazy_load()
-			require("luasnip.loaders.from_lua").load({ paths = "./lua/luasnippets/" })
+			--require("luasnip.loaders.from_lua").load({ paths = "./lua/luasnippets/" })
 			require("blink.cmp").setup(opts)
 		end,
 	},
